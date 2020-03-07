e00866402430de2151aae7d75fa703bbcea9e430
RedeemlessKills (JP) <me@redeemlesskills.tk>
2020-03-07 09:01:53 -0500

    Fixed not saving

--- password.py
+++ password.py
@@ -1,9 +1,9 @@
 import hashlib
 def main():
     FN = input("Enter file location: ")
+    file = open(FN, "r")
     text = input("Input your text here: ")
     salt = input("Enter Salt: ")
-    file = open(FN, "w")
     options = ["[1] Encode Text", "[2] Match Text"]
     optionsIn = [0,1,2]
     a = optionsIn[1]
@@ -23,10 +23,13 @@ def main():
         exit()
 def opt1(FN,file,text,salt,options,optionsIn,a,b,input1,enc,h,hash):
     print(hash)
+    file = open(FN, "w")
     file.write(h.hexdigest())
+    file.close()
 def opt2(FN,file,text,salt,options,optionsIn,a,b,input1,enc,h,hash):
     file = open(FN, "r")
     phash = file.read()
+    file.close()
     if hash == phash:
         print("Success, the password is: " + text)
     else:
